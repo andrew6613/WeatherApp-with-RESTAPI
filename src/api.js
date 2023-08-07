3872b828a8454e06255cbef778d0631676eed45a
andrew6613 <abazarov592@gmail.com>
2023-08-07 21:02:46 +0300

    geoAPI

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f84fcf7fdmsh4401051b2f7c3c1p198930jsn82c269422098",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
--- src/components/SearchLocation.jsx
+++ src/components/SearchLocation.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 
+const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f84fcf7fdmsh4401051b2f7c3c1p198930jsn82c269422098",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
 const SearchLocation = () => {
   const [cityName, setCityName] = useState(null);
 
-  const handleCity = (inputValue) => {
-    setCityName(inputValue);
+  const handleCity = (searhValue) => {
+    setCityName(searhValue);
+  };
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(`${url}?namePrefix=${inputValue}`, options);
+      const result = await response.text();
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -15,6 +34,7 @@ const SearchLocation = () => {
         onChange={handleCity}
         value={cityName}
         debounceTimeout={600}
+        loadOptions={loadOptions}
       />
     </div>
   );
